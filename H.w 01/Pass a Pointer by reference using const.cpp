16461f1dabcca0e1aef562c8747587bf9991a84a
Miss-Arooj <113843418+Miss-Arooj@users.noreply.github.com>
2024-06-26 16:06:48 +0500

    Add files via upload

---
+++ H.w 01/Pass a Pointer by reference using const.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+using namespace std;
+
+void func(const int*& p)
+{
+    *p = 20; //error
+    //p = 20;
+}
+void main()
+{
+    int a = 10;
+    cout << a << endl;
+    int* p1 = &a;
+    func(p1);
+    cout << *p1 << endl;
+}
---
+++ H.w 01/Pass by Reference.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void func(int& a) {
+	a = 10;
+}
+void main() {
+	int a = 5;
+	cout << a << endl;
+	func(a);
+	cout << a << endl;
+}
